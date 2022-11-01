ebb0a98ea464ee8693d303ae9bba23b60ac17d7c
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-11-01 19:24:28 +0300

    task is done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,58 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        String[] data = readDataToString(file).split("\\r\\n|[\\r\\n ]");;
+
+        for (int i = 0; i < data.length; i++) {
+            switch (data[i]) {
+                case "Name:":
+                    profile.setName(data[i+1]);
+                    break;
+                case "Age:":
+                    profile.setAge(Integer.parseInt(data[i+1]));
+                    break;
+                case "Email:":
+                    profile.setEmail(data[i+1]);
+                    break;
+                case "Phone:":
+                    profile.setPhone(Long.parseLong(data[i+1]));
+                    break;
+                default:
+            }
+        }
+
+        return profile;
+    }
+
+    private String readDataToString(File file) {
+        StringBuilder data = new StringBuilder();
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
+            FileChannel inChannel = randomAccessFile.getChannel();
+            long fileSize = randomAccessFile.length();
+
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            for (int i = 0; i < fileSize; i++) {
+                data.append((char) buffer.get());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data.toString();
     }
 }
